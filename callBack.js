6f2e0a9d1d59459dc2f61fa993e583769aea0835
shawon <shawon.softeng@gmail.com>
2023-03-21 13:17:53 +0600

    second commit to callback

--- callBack.js
+++ callBack.js
@@ -1,40 +1,32 @@
 function sample(a, b, callBack) {
-    // const C = a + b;
-    // const D = a - b;
+    const C = a + b;
+    const D = a - b;
 
-    // const res = sum(C, D);
-    // return res;
 
-    // const res2 = callBack(C, D);
-    // return res2;
+    const res2 = callBack(C, D);
+    return res2;
 }
 
-// function sum(x, y) {
-//     return x + y;
-// }
+function sum(x, y) {
+    return x + y;
+}
 
 
 
 // const result2 = sample(12, 10);
 // console.log(result2);
 
-const result = sample(10, 6, subtract);
-
-function subtract(k, l) {
-    return k - l;
-}
-
-// console.log(result);
+const result = sample(10, 6, sum);
+console.log(result);
 
 
 
 function something(A, B, cb) {
     const C = A + B;
     const D = A - B;
-    console.log(C, D);
+
 
     const fRes = cb(C, D);
-    console.log(fRes);
     return fRes;
 
 
